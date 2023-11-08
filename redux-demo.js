4f0933e6415b49c5dfe3000045ed863d91d24f9e
komalkumari <keshrikomal887@gmail.com>
2023-11-08 10:10:54 -0500

    added

--- redux-demo.js
+++ redux-demo.js
@@ -7,10 +7,20 @@ const redux=require('redux');
 const counterReducer=(state={counter:0},action)=>{
     if(action.type === 'inc'){
         return {
-            counter:state.counter + 2
+            counter:state.counter + 1
         }
     }
     if(action.type === 'dec'){
+        return {
+            counter:state.counter-1
+        }
+    }
+    if(action.type ==='INCREMENTBY2 '){
+        return {
+            counter:state.counter+2
+        }
+    }
+    if(action.type === 'DECREMENTBY2 '){
         return {
             counter:state.counter-2
         }
@@ -34,7 +44,14 @@ store.subscribe(CounterSubscriber)
 // store.dispatch({type:'inc'})
 // store.dispatch({type:'inc'})
 // node 
-store.dispatch({type:'inc'})
-store.dispatch({type:'dec'})
+// store.dispatch({type:'inc'})
+// store.dispatch({type:'dec'})
+store.dispatch({type:'DECREMENTBY2'})
+store.dispatch({
+    type:'INCREMENTBY2 '
+})
+store.dispatch({
+    type:'INCREMENTBY2 '
+})
 
 
